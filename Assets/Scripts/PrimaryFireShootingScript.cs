8cb48c07e1891398c810c95e85c4f6e42234bd14
Adam <57605169+Captaj@users.noreply.github.com>
2019-11-15 19:01:54 -0500

    Added Secondary Fire
    Added secondary fire assets and prefab, as well as script. Item showing in window but not in game window. Unknown issue.

--- Assets/Scripts/PrimaryFireShootingScript.cs
+++ Assets/Scripts/PrimaryFireShootingScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 
 
-public class ShootingScript : MonoBehaviour
+public class PrimaryFireShootingScript : MonoBehaviour
 {
 
 
---
+++ Assets/Scripts/SecondaryFireShootingScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SecondaryFireShootingScript : MonoBehaviour
+{
+
+
+    public GameObject secondaryBulletPrefab;
+    public Transform secondaryPlayerMuzzle;
+
+    public float bulletVelocity = 15f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+
+            //if(curAmmo > 0)
+
+            ShootSecondary();
+
+            
+        }
+
+    }
+
+
+    private void ShootSecondary()
+    {
+        GameObject secondaryBullet = Instantiate(secondaryBulletPrefab, secondaryPlayerMuzzle.position, secondaryPlayerMuzzle.rotation);
+        Rigidbody2D rb = secondaryBullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(secondaryPlayerMuzzle.up * bulletVelocity, ForceMode2D.Impulse);
+        Destroy(secondaryBullet, 5f);
+    }
+
+
+}
